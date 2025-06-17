854a078b86616b15e907ff3b0382073988bff1c9
SE0NGH0 <ericsungho@gmail.com>
2025-06-17 10:15:29 +0900

    new

--- 04.process/08.mydaemon.c
+++ 04.process/08.mydaemon.c
@@ -1,76 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <syslog.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <time.h>
-
-void daemonize(void) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
-    }
-    if (pid > 0) {
-        /* Parent exits, child continues */
-        exit(EXIT_SUCCESS);
-    }
-
-    /* Become session leader to detach from terminal */
-    if (setsid() < 0) {
-        perror("setsid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Clear file creation mask */
-    umask(0);
-
-    /* Change working directory to root */
-    if (chdir("/") < 0) {
-        perror("chdir failed");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Close inherited file descriptors */
-    long max_fd = sysconf(_SC_OPEN_MAX);
-    if (max_fd == -1) {
-        max_fd = 1024;
-    }
-    for (long fd = 0; fd < max_fd; fd++) {
-        close(fd);
-    }
-
-    /* Redirect stdin, stdout, stderr to /dev/null */
-    open("/dev/null", O_RDONLY);
-    open("/dev/null", O_RDWR);
-    open("/dev/null", O_RDWR);
-}
+#include <syslog.h>
 
 int main(void) {
-    FILE *logfp;
-
-    daemonize();
-
-    /* Open log file after daemonization */
-    logfp = fopen("/tmp/mydaemon.log", "a+");
-    if (!logfp) {
-        exit(EXIT_FAILURE);
-    }
-
-    /* Periodically write to log */
-    while (1) {
-        time_t now = time(NULL);
-        struct tm *tm_info = localtime(&now);
-        char time_str[64];
-        strftime(time_str, sizeof(time_str), "%a %b %d %T %Y", tm_info);
-        fprintf(logfp, "Mydaemon alive at %s\n", time_str);
-        fflush(logfp);
-        sleep(5);
-    }
-
-    /* Clean up */
-    fclose(logfp);
-    return EXIT_SUCCESS;
+	pid_t pid, sid;
+	int fd, fd0, fd1, fd2;
+	time_t now;
+	
+	if((pid = fork()) != 0) {
+		exit(0);																							//parent exit
+	}
+	sid=setsid();
+	umask(0022); 			
+	chdir("/");
+	close(STDIN_FILENO); close(STDOUT_FILENO); close(STDERR_FILENO);
+	fd = open("/tmp/mydaemon.log", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	dup2(fd, 1); dup2(fd, 2);
+	while(1){
+		time(&now);
+		fprintf(stdout, "Mydaemon alive at %s", ctime(&now));
+		fflush(stdout); 																			/* flush the stream */
+		sleep(5);
+	}
+	return 0;
 }
-
